6c104ce918d679f365381b78ce20c2375ff8a904
Hack Reactor Students <accounts@hackreactor.com>
2015-02-10 23:22:30 -0800

    Attempted to make dancers dance around each other
    Ran up against some difficulties with the signs on the native trig functions.

--- src/swingDancer.js
+++ src/swingDancer.js
@@ -22,35 +22,60 @@ SwingDancer.prototype.step = function(){
   var randomPhoto = this.photos[Math.floor(Math.random() * this.photos.length)];
   this.$node.find("img").attr("src", randomPhoto);
   //this.$node.toggle();
-  //this.$node
-  //console.log(dancers);
 
+  var myPos = this.$node.offset();
+  var myTop = myPos.top;
+  var myLeft = myPos.left;
 
-  // find the closest other dancer and regard them as a partner
-  // var partner;
+  // // find the closest other dancer and regard them as a partner
+  // var $partner;
   // var partnerDist = 1000000;
   // for (var i=0, l= dancers.length; i<l; i++){
-  //   var friend = dancers[i];
-  //   var dist = Math.sqrt(Math.pow((this.top - friend.top), 2) + Math.pow((this.left - friend.left)));
-  //   if (dist < partnerDist && dist > .1){
-  //     partner = friend;
+  //   var $friend = dancers[i].$node;
+  //   var friendPos = $friend.offset();
+  //   var dist = Math.sqrt(Math.pow((myTop - friendPos.top), 2) + Math.pow((myLeft - friendPos.left),2));
+  //   if ( myTop !==0 &&
+  //        friendPos.top !== 0 &&
+  //        dist < partnerDist &&
+  //        dist > 0.1){
+  //     $partner = $friend;
   //     partnerDist = dist;
   //   }
   // }
-  // console.log('my top is:', this.$node.top)
-  // if (partner){
-  //   // var midTop = (this.top + friend.top)/2;
-  //   // var midLeft = (this.left + friend.left)/2;
-  //   // var angle = Math.cot((this.top - midTop)/(this.left - midLeft));
-  //   // var newAngle = Math.cot + .01;
-  //   // var newTop = midTop + (dist/2) *
-  //   // this.$node.animate({
-  //   //   top: '+=' + dVert,
-  //   //   left: '+='+ dHor
-  //   // })
-  //   setPosition(partner.top, partner.left);
+  // console.log('starting at: ', myTop, ', ', myLeft);
+
+  // // if you've found a partner, rotate around them!
+  // if ($partner){
+  //   var pTop = $partner.offset().top;
+  //   var pLeft = $partner.offset().left;
+  //   console.log('my partner: ', pTop, ", ", pLeft;
+
+
+
+  //   var radius = partnerDist / 2;
+  //   // console.log('radius:', radius);
+  // //  var pTop =
+  //   var midTop = (myTop + $partner.offset().top)/2;
+  //   var midLeft = (myLeft + $partner.offset().left)/2;
+  //   console.log('the midpoint is: ', midTop, ', ', midLeft);
+  //   //debugger;
+  //   var x = myTop - midTop;
+  //   var y = myLeft - midLeft;
+  //   var px = pTop - midTop;
+  //   var py = pLeft - midLeft;
+  //   var tangent =  x/y;
+  //   console.log('tangent:', tangent);
+  //   var angle = Math.atan(tangent);
+  //   console.log('angle: ', angle);
+  //   var newAngle = angle + .01;
+  //   console.log('newAngle: ', newAngle);
+  //   var newX = ((radius) * Math.cos(newAngle));
+  //   var newY = ((radius) * Math.sin(newAngle));
+  //   if  then midTop = midTop + ;
+  //   var newLeft = midLeft + ;
+  //   // var newRadius = Math.sqrt(Math.pow(newTop-midTop,2) + Math.pow(newLeft-midLeft,2));
+  //   // console.log(newRadius);
+  //   console.log('moving to', newTop, ', ', newLeft);
+  //   this.setPosition(newTop, newLeft);
   // }
-
-
-
 };
